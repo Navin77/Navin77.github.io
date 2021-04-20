cc2347a81e2b56d5e1389661cc98b16458c21024
navin77 <rnr7200@gmail.com>
2021-04-20 09:55:46 +0530

    create basic structure in App.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+
+        return (
+            <>
+            <nav>
+                <li>Home</li>
+                <li>Projects</li>
+            </nav>
+            <main>
+                <p>
+                    I am Navin Raj from India and I like web developing,
+                    currently learning the reactJS and ExpressJs libraries
+                    and blockchain.......
+                </p>
+            </main>
+            <footer>
+                <p>done by Navin Raj</p>
+            </footer>
+            </>
+        )
+
+    }
+}
+
+export default App;
